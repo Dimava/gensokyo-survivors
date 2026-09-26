3a1629e72b922c4c00f3d0d9dd333900e1807262
Dimava <dimava2@ya.ru>
2026-09-26 12:49:50 +0300

    Build of 8641ae6 (v2.0.0): key art for the README and the link preview

--- assets/BufferResource-BrTmEN0r.js
+++ assets/BufferResource-BrTmEN0r.js
@@ -1,4 +1,4 @@
-import{w as g,g as x,S as c,u as _,B as y,a as l,E as I,b as m}from"./index-D75eCtBN.js";const M={name:"texture-bit",vertex:{header:`
+import{w as g,g as x,S as c,u as _,B as y,a as l,E as I,b as m}from"./index-BZRzjr4e.js";const M={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
